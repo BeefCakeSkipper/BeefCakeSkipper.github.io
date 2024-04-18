9de7f12d9c754d1cea9c67f6061b0b5b4d62193c
SkipperSmahon <skipper_smahon@hotmail.com>
2024-04-18 12:50:09 +0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.87e8cc083a05bc11a32448eaf0d4517b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8831c7426ee511c4de4b",
+    "url": "/static/css/main.29046914.chunk.css"
+  },
+  {
+    "revision": "8831c7426ee511c4de4b",
+    "url": "/static/js/main.8831c742.chunk.js"
+  },
+  {
+    "revision": "6d5011ca78e5fe9fcc3a",
+    "url": "/static/js/1.6d5011ca.chunk.js"
+  },
+  {
+    "revision": "9b67f8eacc398732386a",
+    "url": "/static/js/2.9b67f8ea.chunk.js"
+  },
+  {
+    "revision": "ff52c985fcc41f0de2a7",
+    "url": "/static/js/runtime~main.ff52c985.js"
+  },
+  {
+    "revision": "4034f65eae93b7b354240295ca4bad7b",
+    "url": "/index.html"
+  }
+];
